e7856af5d43564ce969e688ccaa6c7519cabbe33
Samu2021 <83769078+Samu2021@users.noreply.github.com>
2021-05-12 18:22:38 +0200

    "Correction of the direction"

--- src/logik/Snake.java
+++ src/logik/Snake.java
@@ -26,16 +26,16 @@ public class Snake {
     public int move(int x, int y) {
         switch (currentDirection) {
             case "Up":
-                window.setX[0] = window.setX[0] + window.getBlocksize; //TO-DO: getter- und setter-Funktionen von Window
+                window.setY[0] = window.setY[0] + window.getBlocksize; //TO-DO: getter- und setter-Funktionen von Window
                 break;
             case "Down":
-                window.setX[0] = window.setX[0] - window.getBlocksize;
-                break;
-            case "Left":
                 window.setY[0] = window.setY[0] - window.getBlocksize;
                 break;
+            case "Left":
+                window.setX[0] = window.setX[0] - window.getBlocksize;
+                break;
             case "Right":
-                window.setY[0] = window.setY[0] + window.getBlocksize;
+                window.setX[0] = window.setX[0] + window.getBlocksize;
                 break;
         }
         return 0;
