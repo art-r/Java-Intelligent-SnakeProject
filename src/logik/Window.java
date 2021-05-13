6871e97254c5c9284cc27c11909b94fb6990bb79
Samu2021 <83769078+Samu2021@users.noreply.github.com>
2021-05-13 17:37:47 +0200

    "Static for acces"

--- src/logik/Window.java
+++ src/logik/Window.java
@@ -1,28 +1,50 @@
 package logik;
 
-public class Window  {
-    private int windowHeight = 750;
-    private int windowWidth = 1300;
-    private int windowSize = windowHeight * windowWidth;
+import javax.swing.*;
+import java.awt.*;
 
-    private int boxLength = 50;
-    private int boxSize = boxLength * boxLength;
+public class Window extends JPanel {
+    private static int windowHeight = 750;
+    private static int windowWidth = 1300;
+    private static int windowSize = windowHeight * windowWidth;
 
-    private int numberOfBoxes = windowSize / boxSize;
+    private static int boxLength = 50;
+    private static int boxSize = boxLength * boxLength;
 
-    public int getWindowHeight() {
+    private static int numberOfBoxes = windowSize / boxSize;
+
+    public static int getWindowHeight() {
         return windowHeight;
     }
 
-    public int getWindowWidth() {
-        return windowWidth;
+    public static int getBoxSize() {
+        return boxSize;
     }
 
-    public int getNumberOfBoxes() {
-        return numberOfBoxes;
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
 
-    /*GamePanel() {
+    public void draw(Graphics g) {
+        drawApple(g);
+        drawSnake(g);
+    }
 
-    }*/
+    public static void drawApple(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(Apple.getxCoordinate(), Apple.getyCoordinate(), boxLength, boxLength);
+    }
+
+    public static void drawSnake(Graphics g) {
+        for (int i = 0; i < Snake.getBodypartX().size(); i++) {
+            if (i == 0) { //Kopf evtl. andere Farbe
+                g.setColor(Snake.getCurrentColor());
+            } else {
+                g.setColor(Snake.getCurrentColor());
+            }
+            g.fillRect(Snake.getBodypartX().get(i), Snake.getBodypartY().get(i), boxLength, boxSize);
+        }
+    }
 }
