70633bd60d8871dcba22390e951f12e2e0c38b52
Artur <ay@local.com>
2021-05-14 14:26:07 +0200

    spawn apple fix

--- src/logik/Apple.java
+++ src/logik/Apple.java
@@ -1,12 +1,14 @@
 package logik;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Apple {
     private int xCoordinate;
     private int yCoordinate;
     private final Random RANDOM_NUMBER_GENERATOR = new Random();
+    private boolean newAppleCheck = true;
 
     // these variables are information from the window class (passed through the manager)
     private int windowHeight;
@@ -17,9 +19,25 @@ public class Apple {
         this.windowBoxSize = windowBoxSize;
     }
 
-    public void generateNewApple() {
-        xCoordinate = RANDOM_NUMBER_GENERATOR.nextInt((int)(windowHeight/windowBoxSize)) * windowBoxSize;
-        yCoordinate = RANDOM_NUMBER_GENERATOR.nextInt((int)(windowHeight/windowBoxSize)) * windowBoxSize;
+    public void generateNewApple(ArrayList<Integer> forbiddenX, ArrayList<Integer> forbiddenY) {
+        while (newAppleCheck) {
+            xCoordinate = RANDOM_NUMBER_GENERATOR.nextInt((int) (windowHeight / windowBoxSize)) * windowBoxSize;
+            yCoordinate = RANDOM_NUMBER_GENERATOR.nextInt((int) (windowHeight / windowBoxSize)) * windowBoxSize;
+
+            if (forbiddenX != null) {
+                if ((forbiddenX.stream().filter(s -> s == xCoordinate).count()) == 0) {
+                    if (forbiddenY != null) {
+                        if ((forbiddenY.stream().filter(s -> s == yCoordinate).count()) == 0) {
+                            newAppleCheck = false;
+                        }
+                    } else {
+                        newAppleCheck = false;
+                    }
+                }
+            } else {
+                newAppleCheck = false;
+            }
+        }
     }
 
     public int getxCoordinate() {
