2dd55eb72ecc5ae9e82d58c7228f8a7cd4589e2f
Artur <ay@local.com>
2021-05-20 14:05:10 +0200

    fix snakeHeadX timer bug

--- src/gui/SnakeGame.java
+++ src/gui/SnakeGame.java
@@ -11,12 +11,12 @@ import logik.GameWindow;
 public class SnakeGame {
     public static void main(String[] args) {
         //create a classic snake game (where the user has to play)
-        GameWindow classicSnake = new GameWindow(150);
+        //GameWindow classicSnake = new GameWindow(150);
         //create a robot snake game (where the computer plays)
         //this is robotOne
         RobotMaster robotOne = new SimpleRobot(40);
-        RobotMaster robotTwo = new RobotV2(150);
-        RobotMaster robotThree = new RobotV3(150);
+        //RobotMaster robotTwo = new RobotV2(150);
+        //RobotMaster robotThree = new RobotV3(150);
         //run the game
 
         //the classic game (uncomment to run the classic game)
--- src/logik/GameManager.java
+++ src/logik/GameManager.java
@@ -15,7 +15,7 @@ import java.util.LinkedList;
 public class GameManager extends JPanel implements ActionListener, RobotAPI {
     //create the necessary objects
     private Window window = new Window();
-    private Snake snake = new Snake(window.getBOXLENGTH(), 365);
+    private Snake snake = new Snake(window.getBOXLENGTH(), 6);
     private Apple apple = new Apple(window.getWINDOW_HEIGHT(), window.getWINDOW_WIDTH(), window.getBOXLENGTH(), window.getNUMBER_OF_BOXES());
 
     //this is needed for the game to work (see explanation later on!)
