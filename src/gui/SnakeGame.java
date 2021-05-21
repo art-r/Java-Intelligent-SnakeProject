ec6f1d18155d14abe50f0e18821e8d7ee28fd5d2
Artur <ay@local.com>
2021-05-21 14:47:21 +0200

    update comments

--- src/gui/SnakeGame.java
+++ src/gui/SnakeGame.java
@@ -10,8 +10,8 @@ import logik.GameWindow;
 //the main function to run the game
 public class SnakeGame {
     public static void main(String[] args) {
-        //create the game and pass along the framerate and the gametype (empty String = classic game, RobotV1, RobotV2, RobotV3)
-        GameWindow snake = new GameWindow(100, "RobotV4", true);
+        //create the game and pass along the framerate and the gametype (empty String = classic game, RobotV1, RobotV2, RobotV3, RobotV4)
+        GameWindow snake = new GameWindow(2, "RobotV1", false);
         //Highscore RobotV4: 50
         //Highscore RobotV3: 21
         //Highscore RobotV2: 16
