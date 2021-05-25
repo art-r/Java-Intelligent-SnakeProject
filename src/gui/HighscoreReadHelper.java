88652f26aa324be8bd21898319bf582b11dd98a5
Artur <ay@local.com>
2021-05-25 12:52:05 +0200

    add comments

--- src/gui/HighscoreReadHelper.java
+++ src/gui/HighscoreReadHelper.java
@@ -4,6 +4,9 @@ import logik.HighscoreV2;
 
 import java.util.ArrayList;
 
+//class that helps the start gui to get the highscore values (as in the beginning theres no full highscore manager implemented)
+//possible game-types:
+//RobotV1, RobotV2, RobotV3, RobotV4 or Classic
 public class HighscoreReadHelper {
     HighscoreV2 highscoreManager = new HighscoreV2();
 
